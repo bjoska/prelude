1267e34d1e2335d9993b01d11d6dcb4a58248957
bjoska <bjorn.skarner@it.cdon.com>
2014-01-31 10:07:35 +0100

    Prelude merge.

---
+++ personal/02-xmpfilter.el
@@ -0,0 +1,10 @@
+;;; package Rcodetools
+
+
+(require 'rcodetools)
+;;; Code:
+(define-key ruby-mode-map (kbd "C-c C-c") 'xmp)
+
+(provide '02-xmpfilter.el)
+
+;;; Ends here
---
+++ snippets/cperl-mode/.yas-compiled-snippets.el
@@ -0,0 +1,2 @@
+;;; Compiled snippets and support files for `cperl-mode'
+;;; Do not edit! File generated at Thu Oct 17 10:35:33 2013
